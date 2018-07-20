a164c31ac2b4bf698b9a7613540668731164867c
Rahul Kumar <rathod.vijeta06@gmail.com>
2018-07-20 23:17:43 +0530

    heroku build failed fix

--- server.js
+++ server.js
@@ -6,4 +6,5 @@ const app = express();
 app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default
 // Heroku port
+console.log("Listening at Port",process.env.PORT);
 app.listen(process.env.PORT || 8080);
